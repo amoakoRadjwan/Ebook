02363b3eccbc9bd8063b20e595f2de1b50b1ec86
aiti <aiti@sel-Aspire-3410.(none)>
2011-06-27 12:35:56 +0000

    addd.py

---
+++ e_book/ebook/admin.py
@@ -0,0 +1,7 @@
+from Ebook.models import Book
+from Ebook.models import UploadModel
+from Ebook.models import Search
+from django.contrib import admin
+
+admin.site.register(Book)
+admin.site.register(UploadModel)
