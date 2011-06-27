0a3f95295b53e8d399e86481eda5447d1e361c90
Amoako Radjwan <nanayawrid1@yahoo.com>
2011-06-27 01:54:50 +0000

    lots of stuff

--- e_book/ebook/models.py
+++ e_book/ebook/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib import admin
 class Book(models.Model):
 	title=models.CharField(max_length=200)
 	author=models.CharField(max_length=200)
@@ -7,15 +7,22 @@ class Book(models.Model):
 		return self.title
 
 class UploadModel(models.Model):
-	file = models.FileField(upload_to='home/aiti/Desktop/')
+	file = models.FileField(upload_to='home/aiti /Desktop/')
      	date_uploaded=models.DateTimeField('date uploaded')
+	upload_title = models.CharField(max_length=50)
+	uplaod_author=models.CharField(max_length=50)
 	def __unicode__(self):
-		return self.file
+		return self.upload_title
 
 
 class Search(models.Model):
     	search_title=models.ForeignKey(Book)
     	search_author=models.CharField(max_length=200)    
 	def __unicode__(self):
-		return self.search_title
+		return self.search_author
+
+
+admin.site.register(Search)
+admin.site.register(Book)
+admin.site.register(UploadModel)
 # Create your models here.
--- e_book/ebook/views.py
+++ e_book/ebook/views.py
@@ -1,1 +1,4 @@
 # Create your views here.
+def download(request,id):
+	d=ebook.object.get(pk=id)
+def downlaod(request):
